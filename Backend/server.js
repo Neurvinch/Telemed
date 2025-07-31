6b6e8e5e48cbcb9b9c65f5fe758f146e089da151
Naveen pandian <npandian515@gmail.com>
2025-07-31 20:35:09 +0530

    bhu

--- Backend/server.js
+++ Backend/server.js
@@ -5,4 +5,13 @@ const dotenv = require('dotenv');
 
 const index = require('./index');
 
+  const db = process.env.DB;
 
+  mongoose.connect(db)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
