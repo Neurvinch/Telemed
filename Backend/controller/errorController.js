a845b489b234acd2762853b3b7a0e7266a7d2bf1
Naveen pandian <npandian515@gmail.com>
2025-08-01 15:56:18 +0530

    hjl

--- Backend/controller/errorController.js
+++ Backend/controller/errorController.js
@@ -10,5 +10,23 @@ const {
 } = errorHnadlers;
 
 module.exports = (err, req, res, next) => {
+
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    let error = { ...err, message: err.message};
+
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);   
+    if (err.name === 'JsonWebTokenError') error = handleJWTError();
+    if (err.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
+
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        ...(process.env.NODE_ENV === 'development' && { error, stack: error.stack })
+    });
     
-}
+};
