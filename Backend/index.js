26509caf19022a36583bbaba796ffa93689d76f4
Naveen pandian <npandian515@gmail.com>
2025-08-01 22:10:06 +0530

    huy

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
 
+const globalErrorHandler = require('./controller/errorController')
+
+const AppError = require('./appError')
 
 const app = express();
 
