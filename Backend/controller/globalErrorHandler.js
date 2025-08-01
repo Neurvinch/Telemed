1fe5f878e504f2228a160c86f5edf9607425d9c0
Naveen pandian <npandian515@gmail.com>
2025-08-01 13:52:49 +0530

    n

--- Backend/controller/globalErrorHandler.js
+++ Backend/controller/globalErrorHandler.js
@@ -1,5 +1,7 @@
 const AppError = require ('../appError');
 
 const handleCastErrorDB = (err) => {
-  
-} 
+  const message = `Invalid ${err.path}: ${err.value}. Please provide a valid value.`;
+  return new AppError(message, 400);
+} ;
+
